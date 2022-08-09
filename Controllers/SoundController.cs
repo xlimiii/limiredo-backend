25c46810a849680a52020e88d41d14a2d8635a08
Dominika Limanówka <47063149+xlimiii@users.noreply.github.com>
2022-08-09 23:45:13 +0200

    concatenation of sounds from azure blob and returning them as interval file

--- Controllers/SoundController.cs
+++ Controllers/SoundController.cs
@@ -1,9 +1,14 @@
 ﻿using limiredo_backend.Repositories.Interfaces;
+using limiredo_backend.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace limiredo_backend.Controllers
@@ -12,13 +17,15 @@ namespace limiredo_backend.Controllers
     [Route("[controller]")]
     public class SoundController : ControllerBase
     {
+        private readonly IConfiguration Configuration;
         private readonly ISoundRepository soundRepository;
         private readonly ILogger<SoundController> logger;
 
-        public SoundController(ISoundRepository soundRepository, ILogger<SoundController> logger)
+        public SoundController(ISoundRepository soundRepository, ILogger<SoundController> logger, IConfiguration configuration)
         {
             this.soundRepository = soundRepository;
             this.logger = logger;
+            this.Configuration = configuration;
         }
 
         [HttpGet("{id}")]
@@ -38,15 +45,22 @@ namespace limiredo_backend.Controllers
         [HttpGet("random")]
         public async Task<IActionResult> GetRandomSounds(int count=2)
         {
-            var result = await soundRepository.GetRandomSoundsAsync(count);
-            if (result.IsSuccess == true)
+            var randomSounds = await soundRepository.GetRandomSoundsAsync(count);
+           
+            if (randomSounds.IsSuccess == true)
             {
-                return Ok(result.Sounds);
+                var intervalFile = await SoundService.GetIntervalFile(Configuration["AzureData:Url"], Configuration["AzureData:SAS"], randomSounds.Sounds);
+                return File(intervalFile, "audio/wav", "test.wav");
             }
             else
             {
                 return NotFound();
             }
         }
+
+
+       
+
+    
     }
 }
---
+++ Services/SoundService.cs
@@ -0,0 +1,87 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace limiredo_backend.Services
+{
+    public static class SoundService
+    {
+        public static async Task<byte[]> GetIntervalFile(string url, string sasValue, List<Models.Sound> sounds)
+        {
+            List<byte> resultList = new List<byte>();
+
+            foreach (var sound in sounds)
+            {
+                var file = await ReadBlobWithSasAsync(new Uri($"{url}{sound.File}?{sasValue}"));
+
+
+                MemoryStream ms = new MemoryStream();
+                file.Content.CopyTo(ms);
+                var soundByteArray = ms.ToArray();
+                resultList.AddRange(soundByteArray.Skip(44).ToList());
+            }
+            var memoryStreamWithHeader = new MemoryStream(new byte[44 + resultList.Count]);
+            var memoryStreamWithoutHeader = new MemoryStream(resultList.ToArray());
+            memoryStreamWithHeader.Position = 44;
+            memoryStreamWithoutHeader.CopyTo(memoryStreamWithHeader);
+
+            return CreateWavHeader(memoryStreamWithHeader);
+        }
+
+        private static async Task<BlobDownloadInfo> ReadBlobWithSasAsync(Uri sasUri)
+        {
+            BlobClient blobClient = new BlobClient(sasUri, null);
+            try
+            {
+                BlobDownloadInfo blobDownloadInfo = await blobClient.DownloadAsync();
+                return blobDownloadInfo;
+            }
+            catch (RequestFailedException e)
+            {
+                 throw;
+            }
+        }
+
+        private static byte[] CreateWavHeader(MemoryStream stream)
+        {
+            stream.Position = 0;
+            stream.Write(Encoding.ASCII.GetBytes("RIFF"), 0, 4);
+            stream.Position = 4;
+            stream.Write(BitConverter.GetBytes(0), 0, 4);
+            stream.Position = 8;
+            stream.Write(Encoding.ASCII.GetBytes("WAVE"), 0, 4);
+            stream.Position = 12;
+            stream.Write(Encoding.ASCII.GetBytes("fmt "), 0, 4);
+            stream.Position = 16;
+            stream.Write(BitConverter.GetBytes(16), 0, 4);
+            stream.Position = 20;
+            stream.Write(BitConverter.GetBytes(1), 0, 2);
+            stream.Position = 22;
+            stream.Write(BitConverter.GetBytes(2), 0, 2);
+            stream.Position = 24;
+            stream.Write(BitConverter.GetBytes(44100), 0, 4);
+            stream.Position = 28;
+            stream.Write(BitConverter.GetBytes(176400), 0, 4);
+            stream.Position = 32;
+            stream.Write(BitConverter.GetBytes(4), 0, 2);
+            stream.Position = 34;
+            stream.Write(BitConverter.GetBytes(16), 0, 2);
+            stream.Position = 36;
+            stream.Write(Encoding.ASCII.GetBytes("data"), 0, 4);
+            stream.Position = 40;
+            stream.Write(BitConverter.GetBytes(0), 0, 4);
+
+            stream.Position = 4;
+            stream.Write(BitConverter.GetBytes(stream.Length), 0, 4);
+            stream.Position = 40;
+            stream.Write(BitConverter.GetBytes(stream.Length - 40), 0, 4);
+            return stream.ToArray();
+        }
+    }
+}
